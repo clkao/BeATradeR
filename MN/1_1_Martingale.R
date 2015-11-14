8bf25bd792d99b5745747c11e8e5ee293941c319
Chia-liang Kao <clkao@clkao.org>
2015-11-14 09:53:48 +0800

    Indent

--- MN/1_1_Martingale.R
+++ MN/1_1_Martingale.R
@@ -9,14 +9,16 @@ bid=1				## bidding 1 chip when winning
 while (capital>=bid && i<=num){	## the requirement of bidding on the table
 rout=sample(0:1,size=1,prob=c(19/37,18/37),replace=T)	##rouletee simulation
 
-	if(rout==1){PL[i]=bid	## win "bid" chips
-	bid=1	 		## bidding 1 chip in the next game
-	}else{PL[i]=-bid    	## lossing "bid" chips
-	bid=bid*2}		## double bid
-	
-capital=capital+PL[i]		## the capital 
-i=i+1}				## go to the next game
-
+    if(rout==1) {
+        PL[i]=bid	## win "bid" chips
+        bid=1	 		## bidding 1 chip in the next game
+    } else {
+        PL[i]=-bid    	## lossing "bid" chips
+	bid=bid*2	## double bid
+    }
+    capital=capital+PL[i]		## the capital 
+    i=i+1				## go to the next game
+}
 
 plot(init+cumsum(PL),lwd=3,font=2,type="l",col="red"
 ,xlab="The # of Bidding",ylab="Cumulative Chips",main="Martingale")
